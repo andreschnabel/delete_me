a4e0dbee1b7226914b46110eb4fbc5558e124bda
Andre Schnabel <andreschnabel@me.com>
2013-11-06 12:03:41 +0100

    Iniital

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeleteMe::Application.config.secret_token = 'b1201577b765206e235744bffe3a3a55c3771a05d6c7309051d2502e4cad8f799bdbd9b4d7390158f48c5b3a7918028d2453d1e9256c4ca0546ddc4a7ad48183'
